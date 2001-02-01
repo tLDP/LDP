7b7bacbdde0844dae8a874cb11e197a49087bc7d
serek <>
2001-02-01 18:42:06 +0000

    - initial version from current design.

---
+++ LDP/www/list.linuxdoc.org/index.php
@@ -0,0 +1,111 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
+<meta http-equiv="Content-Language" content="pl">
+<title>list.linuxdoc.org</title>
+</head>
+
+<body bgcolor="#ffffff" link="#008000" vlink="#008000" alink="#008000">
+<TBODY>
+<table border="0" width="640" cellpadding="11">
+  <tr>
+    <td width="100%" colspan="2"><font face="Verdana">
+	<h2><a href="http://linuxdoc.org/">linuxdoc</a>:
+	e-lists</h2></font></td>
+  </tr>
+  <tr>
+  <tr>
+    <td width="100%" colspan="2"><font face="Verdana">Welcome. 
+    You are watching our public lists.
+    This archive is <a href="/search/">searchable</a>. You could also check 
+    <a href="http://gabber.metalab.unc.edu/">the listserver parameters</a>.
+    </td>
+  </tr>
+  <tr>
+    <td width="100%" colspan="2"><font face="Verdana">
+    If you want to subscribe our lists,
+    please send an empty message to: <br>
+    <br><i>LIST_NAME</i>-subscribe@linuxdoc.org<br>
+    <br>eg. for announce list you should use the following address:<br>
+    <br><b>announce-subscribe@linuxdoc.org</b>
+    </td>
+  </tr>
+  <tr>
+    <td width="100%" colspan="2"><font face="Verdana">
+    If you want to unsubscribe, please check headers of any 
+    message from list in your mailbox, there is info about 
+    proper address. Usually it is: <br>
+    <br><i>LIST_NAME</i>-unsubscribe@linuxdoc.org
+    </td>
+  </tr>
+  <tr>
+    <td>
+	<p><a href="/archive/index.htm?1">discuss</a></p>
+    </td>
+    <td>
+	<p>general developers and documentation producers discuss</p>
+  </tr>
+  <tr>
+    <td>
+	<p><a href="/archive/index.htm?2">docbook</a></p>
+    </td>
+    <td>
+	<p>docbook related problems</p>
+    </td>
+  </tr>
+  <tr>
+    <td>
+        <p><a href="/archive/index.htm?3">announce</a></p>
+    </td>
+    <td>
+        <p>linuxdoc announces - small volume list</p>
+    </td>
+  </tr>
+  <tr>
+     <td>
+        <p><a href="/archive/index.htm?4">cvs-commits</a></p>
+     </td>
+     <td>
+        <p>commits into our repository</p>
+     </td>
+  </tr>
+  <tr>
+     <td>
+        <p><a href="/archive/index.htm?5">submit</a></p>
+     </td>
+     <td>
+        <p>general submissions address</p>
+     </td>
+  </tr>
+  <tr>
+     <td>
+        <p><a href="/archive/index.htm?7">feedback</a></p>
+     </td>
+     <td>
+        <p>say what do you think about linuxdoc?</p>
+     </td>
+  </tr>
+  <tr>
+     <td>
+        <p><a href="/archive/index.htm?6">wheel</a></p>
+     </td>
+     <td>
+        <p>'just testing' list - please ignore</p>
+     </td>
+  </tr>
+<td colspan="2">If you are list owner, you can <a href="/admin/">manage your lists</a>.</td>
+</tr><tr>
+<td allign="top">
+<a href="http://ibiblio.org/"><img border="0" src="logo7.jpg"></a>
+</td>
+<td allign="top">
+<i><font face="Verdana"><small>This site is designed & maintained by <a 
+	href="http://metalab.unc.edu/ser/">Sergiusz Pawłowicz</a> &lt;<a 
+        href="mailto:ser@metalab.unc.edu">ser@metalab.unc.edu</a>&gt;</small></font></i>
+</td>
+</tr>
+</table>
+</TBODY>
+</body>
+</html>
