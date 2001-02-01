6dca7b71cf7cb024dfa12cf09a7b0f320deb17cd
serek <>
2001-02-01 18:54:07 +0000

    - added url to old archives.

--- LDP/www/list.linuxdoc.org/index.php
+++ LDP/www/list.linuxdoc.org/index.php
@@ -94,8 +94,14 @@
         <p>'just testing' list - please ignore</p>
      </td>
   </tr>
-<td colspan="2">If you are list owner, you can <a href="/admin/">manage your lists</a>.</td>
-</tr><tr>
+<tr>
+	<td colspan="2"><a href="/old/">
+	Old archives to December 2000</a></td>
+</tr>
+<tr>
+	<td colspan="2">If you are list owner, you can 
+	<a href="/admin/">manage your lists</a>.</td>
+</tr>
 <td allign="top">
 <a href="http://ibiblio.org/"><img border="0" src="logo7.jpg"></a>
 </td>
