d69e814a813231d1227f99d10f4e5731aeb35444
david <>
2002-03-26 00:44:58 +0000

    load in alphabetical order (it's easier to make sure load.sql is correct)

--- LDP/lampadas/database/load.sql
+++ LDP/lampadas/database/load.sql
@@ -1,36 +1,28 @@
+copy audience from '/tmp/ldp_audience.txt';
 copy class from '/tmp/ldp_class.txt';
+copy config from '/tmp/ldp_config.txt';
+copy doc_vote from '/tmp/ldp_doc_vote.txt';
+copy document from '/tmp/ldp_document.txt';
+copy document_audience from '/tmp/ldp_document_audience.txt';
+copy document_editor from '/tmp/ldp_document_editor.txt';
+copy document_maintainer from '/tmp/ldp_document_maintainer.txt';
+copy document_rev from '/tmp/ldp_document_rev.txt';
+copy document_topic from '/tmp/ldp_document_topic.txt';
+copy document_wiki from '/tmp/ldp_document_wiki.txt';
 copy dtd from '/tmp/ldp_dtd.txt';
+copy editor from '/tmp/ldp_editor.txt';
+copy editor_role from '/tmp/ldp_editor_role.txt';
 copy format from '/tmp/ldp_format.txt';
+copy license from '/tmp/ldp_license.txt';
+copy maintainer from '/tmp/ldp_maintainer.txt';
+copy maintainer_notes from '/tmp/ldp_maintainer_notes.txt';
+copy notes from '/tmp/ldp_notes.txt';
 copy pub_status from '/tmp/ldp_pub_status.txt';
 copy review_status from '/tmp/ldp_review_status.txt';
 copy role from '/tmp/ldp_role.txt';
-copy topic from '/tmp/ldp_topic.txt';
-copy subtopic from '/tmp/ldp_subtopic.txt';
-
-copy document from '/tmp/ldp_document.txt';
-
-copy notes from '/tmp/ldp_notes.txt';
-
-copy document_wiki from '/tmp/ldp_document_wiki.txt';
-copy document_topic from '/tmp/ldp_document_topic.txt';
-copy document_rev from '/tmp/ldp_document_rev.txt';
-
-copy maintainer from '/tmp/ldp_maintainer.txt';
-copy maintainer_notes from '/tmp/ldp_maintainer_notes.txt';
-copy document_maintainer from '/tmp/ldp_document_maintainer.txt';
-
-copy editor from '/tmp/ldp_editor.txt';
-copy editor_role from '/tmp/ldp_editor_role.txt';
-copy document_editor from '/tmp/ldp_document_editor.txt';
-
-copy audience from '/tmp/ldp_audience.txt';
-copy document_audience from '/tmp/ldp_document_audience.txt';
-
-copy doc_vote from '/tmp/ldp_doc_vote.txt';
-
 copy stats from '/tmp/ldp_stats.txt';
 copy stats_cdf from '/tmp/ldp_stats_cdf.txt';
-copy license from '/tmp/ldp_license.txt';
-
+copy subtopic from '/tmp/ldp_subtopic.txt';
+copy topic from '/tmp/ldp_topic.txt';
 copy username from '/tmp/ldp_username.txt';
-copy config from '/tmp/ldp_config.txt';
+
