a09c64a03fd9b54e6543d4874926ab221aa5c46f
antoviaque <>
2002-04-21 17:05:38 +0000

    - Replaced <SPACES> by <TABS> (some "for i in `find -name \*.py` ; do  sed -e "s/    /       /" $i > ${i}.2 ; mv ${i}.2 $i ; echo $i ; done" in lib/lampadas/ and www/). - Fixed some notation inconsistencies (variables) and typos. - unittest now works, but I had a strange freezing problem with SQL_DROP when testing. I'll work on it. Completed the existing test. - Integrated David's user_id change in username table.

--- LDP/lampadas/lib/lampadas/__unittest_data__.py
+++ LDP/lampadas/lib/lampadas/__unittest_data__.py
@@ -8,62 +8,71 @@ import DataLayer
 DBNAME = 'db_test'
 
 def get_connection(name) :
-    from pyPgSQL import PgSQL
-    return PgSQL.connect(database=name)
+	from pyPgSQL import PgSQL
+	return PgSQL.connect(database=name)
 
 SQL_DROP = "DROP TABLE username"
 
 SQL_CREATE = """CREATE TABLE username (
-	username	CHAR(20)	NOT NULL,
-	first_name	CHAR(20),
-	surname		CHAR(20),
-	maintainer_id	INT4,
-	email		TEXT,
-	admin		BOOLEAN,
-	editor_id	INT4,
+    user_id     INT4        NOT NULL,
+	username    CHAR(20)    NOT NULL UNIQUE,
+	session_id  CHAR(20),
+	first_name  CHAR(20),
+	middle_name CHAR(20),
+	surname     CHAR(20),
+	email       TEXT,
+	admin       BOOLEAN,
+	password    CHAR(12),
+	notes       TEXT,
 
-	PRIMARY KEY (username)
+	PRIMARY KEY (user_id)
 )
 """
 
 SQL_INSERT = """
-INSERT INTO username ( username, first_name, surname, email )
-VALUES ( %(username)s, %(first_name)s, %s(surname), %(email)s )
+INSERT INTO username (user_id, username, first_name, surname, email, admin )
+VALUES ('1', '%(username)s', '%(first_name)s', '%(surname)s', '%(email)s', TRUE )
 """
 
 DATA_INSERT = {'username':'david',
-               'first_name':'David', 'surname':'Merrill',
-               'email':'david@lupercalia.net'}
+			   'first_name':'David', 'surname':'Merrill',
+			   'email':'david@lupercalia.net'}
 
 DATA_TUPLE = ('david', 'David', 'Merrill', '12', 'david@lupercalia.net',
-              '1', '13' )
+			  '1', '13' )
 
 # tests ########################################################################
 
 class DatabaseTest(unittest.TestCase) :
 
-    def setUp(self):
-        self.con = get_connection(DBNAME)
-        try:
-            self.con.cursor().execute(SQL_CREATE)
-        except Exception, e :
-            print e
-            self.con.cursor().execute('delete from %s'%TABLE)
-        self.con.commit()
-        self.con.cursor().execute(SQL_INSERT, DATA_INSERT)
-        self.con.commit()
-        self.db = DataLayer.PgSQLDatabase(DBNAME)
+	def setUp(self):
+		self.con = get_connection(DBNAME)
+		try:
+			self.con.cursor().execute(SQL_CREATE)
+		except Exception, e :
+			print e
+			self.con.cursor().execute('delete from username')
+		self.con.commit()
+		self.con.cursor().execute(SQL_INSERT % DATA_INSERT)
+		self.con.commit()
+		self.db = DataLayer.get_database('pgsql', DBNAME)
 
-    def tearDown(self) :
-        self.con.cursor().execute(DROP_TABLE)
-        self.con.commit()
-        self.con.close()
-        
-    def test_get_user(self):
-        u = self.db.get_user('david')
-        self.assert_(isinstance(u,User))
-        self.assert_(u.name == 'david')
-        # etc.
+	def tearDown(self) :
+	#	self.con.cursor().execute(SQL_DROP)
+	#	self.con.commit()
+	#	self.con.close()
+		pass
+
+	def test_get_user(self):
+		u = self.db.get_user_by_name('david')
+		self.assert_(isinstance(u,DataLayer.User))
+		self.assert_(u.username == 'david')
+		self.assert_(u.firstname == 'David')
+		self.assert_(u.surname == '')
+		self.assert_(u.name == 'Merrill')
+		self.assert_(u.email == 'david@lupercalia.net')
+		self.assert_(u.is_admin == 1)
+		
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
