4639663e601feb723759ada1536df2a7d07899fd
david <>
2002-05-03 12:32:56 +0000

    string_id to string_code bugfix

--- LDP/lampadas/database/indexes.sql
+++ LDP/lampadas/database/indexes.sql
@@ -22,7 +22,7 @@ ALTER TABLE doc_vote		ADD CONSTRAINT doc_id_fk	FOREIGN KEY (doc_id)			REFERENCES
 ALTER TABLE doc_vote		ADD CONSTRAINT user_id_fk	FOREIGN KEY (user_id)			REFERENCES username(user_id);
 ALTER TABLE notes		ADD CONSTRAINT doc_id_fk	FOREIGN KEY (doc_id)			REFERENCES document(doc_id);
 ALTER TABLE notes		ADD CONSTRAINT creator_id_fk	FOREIGN KEY (creator_id)		REFERENCES username(user_id);
-ALTER TABLE string_i18n		ADD CONSTRAINT string_id_fk	FOREIGN KEY (string_id)			REFERENCES string(string_id);
+ALTER TABLE string_i18n		ADD CONSTRAINT string_code_fk	FOREIGN KEY (string_code)		REFERENCES string(string_code);
 ALTER TABLE string_i18n		ADD CONSTRAINT string_lang_fk	FOREIGN KEY (lang)			REFERENCES language(isocode);
 ALTER TABLE subtopic		ADD CONSTRAINT topic_num_fk	FOREIGN KEY (topic_num)			REFERENCES topic(topic_num);
 ALTER TABLE username_notes	ADD CONSTRAINT user_id_fk	FOREIGN KEY (user_id)			REFERENCES username(user_id);
