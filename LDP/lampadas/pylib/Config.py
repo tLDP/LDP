0a97e8be44d3da89f1cd34718ae5330004d3258a
david <>
2002-05-15 02:15:52 +0000

    New config file

---
+++ LDP/lampadas/pylib/Config.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+"""
+Lampadas Configuration module
+
+This module provides configuration information from lampadas.conf.
+"""
+
+# Modules ##################################################################
+
+
+# BaseConfig ###############################################################
+
+class ConfigFileReadErrorException(Exception) :
+	pass
+	
+class Config:
+	"""
+	Basic configuration options (dbname, dbtype), used to know where we can find
+	the database.
+	"""
+
+	DBType = ""
+	DBName = ""
+
+	def __init__(self) :
+		import ConfigParser
+
+		self.config = ConfigParser.ConfigParser()
+		self.config.readfp(open('lampadas.conf'))
+		if not self.config.has_section('DB') :
+			raise ConfigFileReadErrorException("File 'lampadas.conf' is missing or does not contain a '[DB]' section")
+			
+		if not self.config.has_option('DB', 'dbtype') :
+			raise ConfigFileReadErrorException("Can't read option 'dbtype' from lampadas.conf")
+		self.DBType = self.config.get('DB', 'dbtype')
+		
+		if not self.config.has_option('DB', 'dbname') :
+			raise ConfigFileReadErrorException("Can't read option 'dbname' from lampadas.conf")
+		self.DBName = self.config.get('DB', 'dbname')
+		
+
+# main
+if __name__ == '__main__' :
+	print "This module cannot be run from the command line"
