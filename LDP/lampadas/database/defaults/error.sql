958a08535e377cf3638eb7370a26c9ccadc7795d
david <>
2002-05-21 14:46:08 +0000

    default data for the error and error_i18n tables

---
+++ LDP/lampadas/database/defaults/error.sql
@@ -0,0 +1,6 @@
+INSERT INTO error (err_id) VALUES (1);
+INSERT INTO error (err_id) VALUES (2);
+
+INSERT INTO error_i18n (err_id, lang, err_name, err_desc) VALUES (1, 'EN', 'File not found',	'The source file does not exist in the Lampadas cvs cache.');
+INSERT INTO error_i18n (err_id, lang, err_name, err_desc) VALUES (2, 'EN', 'File not writable',	'The source file exists, but is not writable.');
+
