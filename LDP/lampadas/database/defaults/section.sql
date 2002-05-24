d381dbf1b0db0a55f5f1631c54ed9097187144ea
david <>
2002-05-24 02:58:17 +0000

    default values for sections

---
+++ LDP/lampadas/database/defaults/section.sql
@@ -0,0 +1,10 @@
+DELETE FROM section_i18n;
+DELETE FROM section;
+
+INSERT INTO section (section_code) VALUES ('main');
+INSERT INTO section (section_code) VALUES ('misc');
+
+INSERT INTO section_i18n (section_code, lang, section_name) VALUES ('main', 'EN', 'Main Menu');
+INSERT INTO section_i18n (section_code, lang, section_name) VALUES ('main', 'FR', 'Le Main Menu');
+INSERT INTO section_i18n (section_code, lang, section_name) VALUES ('misc', 'EN', 'Misc Menu');
+INSERT INTO section_i18n (section_code, lang, section_name) VALUES ('misc', 'FR', 'Le Misc Menu');
