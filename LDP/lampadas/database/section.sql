f777827d64271223d8e82931897bc2fa35b778a3
david <>
2002-05-24 02:57:53 +0000

    section table scripts

---
+++ LDP/lampadas/database/section.sql
@@ -0,0 +1,8 @@
+DROP TABLE section;
+
+CREATE TABLE section
+(
+	section_code		CHAR(12)	NOT NULL,
+
+	PRIMARY KEY (section_code)
+);
---
+++ LDP/lampadas/database/section_i18n.sql
@@ -0,0 +1,10 @@
+DROP TABLE section_i18n;
+
+CREATE TABLE section_i18n
+(
+	section_code		CHAR(12)	NOT NULL,
+	lang			CHAR(2)		NOT NULL,
+	section_name		TEXT		NOT NULL,
+
+	PRIMARY KEY (section_code, lang)
+);
