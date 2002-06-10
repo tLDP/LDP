8d20c89aa90bea7056164bf0e02393866070d9a7
alba <>
2002-06-10 18:38:08 +0000

    Moved foreign key definition into create.sql

--- LDP/lampadas/database/tables/string_i18n/create.sql
+++ LDP/lampadas/database/tables/string_i18n/create.sql
@@ -1,7 +1,9 @@
 CREATE TABLE string_i18n
 (
-	string_code		CHAR(12)	NOT NULL,
-	lang			CHAR(2)		NOT NULL,
+	string_code		CHAR(12)	NOT NULL
+				REFERENCES string(string_code),
+	lang			CHAR(2)		NOT NULL
+				REFERENCES language(isocode),
 	string			TEXT		NOT NULL,
 
 	PRIMARY KEY (string_code, lang)
--- LDP/lampadas/database/tables/string_i18n/indexes.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE string_i18n		ADD CONSTRAINT string_lang_fk		FOREIGN KEY (lang)			REFERENCES language(isocode);
