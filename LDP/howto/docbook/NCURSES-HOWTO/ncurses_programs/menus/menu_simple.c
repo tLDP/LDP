9d7d19b622b6c997fa7a3c172825d6cdbb0eba47
ppadala <>
2002-06-25 04:43:46 +0000

    getc() and some fancy additions

--- LDP/howto/docbook/NCURSES-HOWTO/ncurses_programs/menus/menu_simple.c
+++ LDP/howto/docbook/NCURSES-HOWTO/ncurses_programs/menus/menu_simple.c
@@ -16,30 +16,31 @@ int main()
 {	ITEM **my_items;
 	int c;				
 	MENU *my_menu;
-        int n_choices, i;
+    int n_choices, i;
 	ITEM *cur_item;
 	
 	
 	initscr();
-        cbreak();
-        noecho();
+    cbreak();
+    noecho();
 	keypad(stdscr, TRUE);
 	
-        n_choices = ARRAY_SIZE(choices);
-        my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+    n_choices = ARRAY_SIZE(choices);
+    my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
 
-        for(i = 0; i < n_choices; ++i)
-                my_items[i] = new_item(choices[i], choices[i]);
+    for(i = 0; i < n_choices; ++i)
+        my_items[i] = new_item(choices[i], choices[i]);
 	my_items[n_choices] = (ITEM *)NULL;
 
 	my_menu = new_menu((ITEM **)my_items);
+	mvprintw(LINES - 2, 0, "F1 to Exit");
 	post_menu(my_menu);
 	refresh();
 
 	while((c = getch()) != KEY_F(1))
-	{       switch(c)
-	        {	case KEY_DOWN:
-				menu_driver(my_menu, REQ_DOWN_ITEM);
+	{   switch(c)
+	    {	case KEY_DOWN:
+		        menu_driver(my_menu, REQ_DOWN_ITEM);
 				break;
 			case KEY_UP:
 				menu_driver(my_menu, REQ_UP_ITEM);
@@ -48,7 +49,7 @@ int main()
 	}	
 
 	free_item(my_items[0]);
-        free_item(my_items[1]);
+    free_item(my_items[1]);
 	free_menu(my_menu);
 	endwin();
 }
