c7f30bb0c9248eda79c04ebfa4e782cbe4337d12
alba <>
2002-06-29 19:06:58 +0000

    echo "Environment variable LAMPADAS_SQL is not set." echo "Read the comments in file 'setenv.sh' how to fix that."

---
+++ LDP/lampadas/bin/setenv.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# First level of configuration
+#
+# Adapt this file to your needs. For a typical installation you
+# have to change only one variable, LAMPADAS_ROOT.
+#
+# Include this file in your ~/.bashrc (e.g. with ". bin/setenv.sh")
+# Users of csh should convert the syntax with
+# 
+#     sed -e 's/^export/setenv/' -e 's/=/ /' setenv.sh > setenv.csh
+#
+# and then include setenv.csh in their .cshrc (e.g. "source bin/setenv.csh)
+
+# directory you unpacked lampadas to
+export LAMPADAS_ROOT=$HOME/cvs/LDP/lampadas
+
+# used by bin/lampadasdb to initialize the database
+export LAMPADAS_SQL=$LAMPADAS_ROOT/database/tables
+
+# used by bin/lampadasweb to find the file lampadas.conf
+export LAMPADAS_ETC=$LAMPADAS_ROOT/conf
