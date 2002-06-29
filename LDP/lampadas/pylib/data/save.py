d712900eae757c5780b1afc4a73f738111b3c096
david <>
2002-06-29 16:48:46 +0000

    stub for mod_python publisher, will save form data

---
+++ LDP/lampadas/pylib/data/save.py
@@ -0,0 +1,18 @@
+
+def main(req):
+    return apache.OK
+
+
+def handler(req):
+    return apache.OK
+
+
+def test(req):
+    
+    return "<html>it worked!</html>"
+    return apache.OK
+
+def test2(req):
+    
+    return "<html>it worked!</html>"
+    return apache.OK
