22d5db44e3c3b428f1933c0f5d108f7428c54a9a
alba <>
2002-07-04 18:40:06 +0000

    Added definition of LAMPADAS_DATA and LAMPADAS_DB

--- LDP/lampadas/bin/setenv.sh
+++ LDP/lampadas/bin/setenv.sh
@@ -13,16 +13,24 @@
 # and then include setenv.csh in their .cshrc (e.g. "source bin/setenv.csh")
 
 # directory you unpacked lampadas to
-export LAMPADAS_ROOT=$HOME/cvs/LDP/lampadas
+export LAMPADAS_ROOT="$HOME/cvs/LDP/lampadas"
 
 # used by bin/lampadasdb to initialize the database
-export LAMPADAS_SQL=$LAMPADAS_ROOT/database/tables
+# location of create.sql and insert.m4
+export LAMPADAS_SQL="$LAMPADAS_ROOT/database/tables"
+
+# use by commands 'insert' and 'update' of bin/lampadasdb
+# to locate data files (e.g. EN.m4, DE.m4).
+export LAMPADAS_DATA="$LAMPADAS_SQL"
 
 # generated files that are not automatically deleted after use
 export LAMPADAS_SPOOL=/tmp
 
+# database name to connect to. see psql(1)
+export LAMPADAS_DB=lampadas
+
 # used by bin/lampadasweb to find the file lampadas.conf
-export LAMPADAS_ETC=$LAMPADAS_ROOT/conf
+export LAMPADAS_ETC="$LAMPADAS_ROOT/conf"
 
 # used by pylib/Makefile.py to locate XSLT stylesheets
-export LAMPADAS_XSL=$LAMPADAS_ROOT/xsl
+export LAMPADAS_XSL="$LAMPADAS_ROOT/xsl"
