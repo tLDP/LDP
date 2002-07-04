9d697a75f9687f603e56ef0974a9ad68f150e31f
david <>
2002-07-04 14:22:35 +0000

    move class to type

--- LDP/lampadas/database/tables/class_i18n/create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE class_i18n
-(
-	class_code	CHAR(20)	NOT NULL	REFERENCES class(class_code),
-	lang		CHAR(2)		NOT NULL	REFERENCES language(lang_code),
-	class_name	CHAR(20)	NOT NULL,
-	class_desc	TEXT		NOT NULL,
-
-	PRIMARY KEY (class_code, lang)
-);
--- LDP/lampadas/database/tables/class_i18n/permissions.sql
+++
@@ -1,2 +0,0 @@
-GRANT ALL ON class_i18n TO "www-data";
-GRANT SELECT ON	class_i18n TO root;
