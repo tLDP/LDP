c221161293001bc4d2c6c94dbffefd063d83c329
nico <>
2002-07-06 16:47:31 +0000

    Now uses urlparse from standard library. Added FIXME.

--- LDP/lampadas/pylib/URLParse.py
+++ LDP/lampadas/pylib/URLParse.py
@@ -23,27 +23,19 @@ The URLParse module deciphers a Lampadas URL into its component parts.
 The API is documented in the Lampadas Programmer's Guide.
 """
 
-import string
 from DataLayer import lampadas
 from Log import log
 import urlparse
 
-def splitterm(term, sep):
-    """
-    Splits the term into 2 pieces according to the seperator,
-    and returns them. If the term does not contain the sep,
-    the second string the returned tuple will be ''.
-    """
-
-    temp = string.split(term, sep, 1)
-    if len(temp) > 1:
-        return temp[0], temp[1]
-    else:
-        return temp[0], ''
-    
+AVAILABLE_LANG = lampadas.languages.keys()
+#AVAILABLE_LANG = ['FR','EN','ES']
+#def log(a,b): pass
 
 class URI:
-
+    """
+    FIXME: explain what the different attributes are used for.
+    """
+    
     def __init__(self, uri):
 
         log(3, "parsing URI: " + uri)
@@ -52,7 +44,7 @@ class URI:
         self.port = ""
         self.lang = "EN"
         self.force_lang = 0
-        self.path = ""
+        self.path = "/"
         self.format = ""
         self.filename = "home"
         self.id = 0
@@ -61,93 +53,84 @@ class URI:
         self.anchor = ""
 
         self.uri = uri
-        #FIXME: use urlparse from the standard library!
-        #protocol, host, path, params, query, fragment = urlparse.urlparse(uri)
-        self.base = uri
-        if self.base[0]=='/':
-            self.base = self.base[1:]
         
-        temp = uri
-        if len(temp)==0:
-            return
-        if temp[0]=='/':
-            temp = temp[1:]
-        
-        if len(temp)==0:
-            return
-        if temp[-1]=='/':
-            temp = temp[:-1]
+        protocol, host, path, params, query, fragment = urlparse.urlparse(uri)
 
-        if temp.find('://') > 0:
-            self.protocol, temp = splitterm(temp, '://')
-            self.server, temp = splitterm(temp,'/')
-            if self.server.find(':') > 0:
-                self.server, self.port = splitterm(self.server, ':')
-        if temp.find('#') > 0:
-            temp, self.anchor = splitterm(temp, '#')
-        if temp.find('?') > 0:
-            temp, self.parameter = splitterm(temp, '?')
+        # protocol, server, port, anchor, parameters
+        self.protocol = protocol
+        if host.find(':') > 0 :
+            self.server, self.port = host.split(':')
+        else :
+            self.server = host
+        self.parameter = query
+        self.anchor = fragment
         
-        temp = string.split(temp, '/')
+        # path
+        self.base = path
+        path = path.split('/')
 
-        if len(temp) > 0:
-            lang = lampadas.languages[temp[0]]
-            if not lang==None:
-                self.lang = temp[0]
-                temp = temp[1:]
-                self.force_lang = 1
-                self.base = self.base[3:]
+        if not path : return
+        if path[0] == '' :
+            path = path[1:]
+
+        if not path : return
+        if path[-1] == '' :
+            path = path[:-1]
+
+        if not path : return
+        if path[0] in AVAILABLE_LANG :
+            self.force_lang = 1
+            self.lang, path = path[0], path[1:]
+            self.base = '/'.join(path)
 
         # this is where we load ids and codes for pages which
         # contain an object and display its attributes.
-        # 
-        if len(temp) > 0:
-            if temp[0]=='editdoc' or temp[0]=='cvslog':
-                self.filename = temp[0]
-                temp = temp[1:]
-                if len(temp) > 0:
-                    self.id = int(temp[0])
-            elif temp[0]=='topic' or temp[0]=='subtopic' or temp[0]=='type':
-                self.filename = temp[0]
-                temp = temp[1:]
-                if len(temp) > 0:
-                    self.code = temp[0]
-            elif temp[0]=='user':
-                self.filename = temp[0]
-                temp = temp[1:]
-                if len(temp) > 0:
-                    self.username = temp[0]
+        if not path : return
+        if path[0] in ('editdoc','cvslog'):
+            self.filename = path[0]
+            path = path[1:]
+            if path :
+                self.id = int(path[0])
+        elif path[0] in ('topic','subtopic','type'):
+            self.filename = path[0]
+            path = path[1:]
+            if path :
+                self.code = path[0]
+        elif path[0] in ('user',):
+            self.filename = path[0]
+            path = path[1:]
+            if path :
+                self.username = path[0]
+        else:
+            if len(path)==1:
+                self.path = '/'
+                if len(path[0]) > 0:
+                    self.filename = path[0]
             else:
-                if len(temp)==1:
-                    self.path = '/'
-                    if len(temp[0]) > 0:
-                        self.filename = temp[0]
-                else:
-                    self.path = string.join(temp[:-1], '/')
-                    if len(temp[-1]) > 0:
-                        self.filename = temp[-1]
-
+                self.path = '/'.join(path[:-1])
+                if len(path[-1]) > 0:
+                    self.filename = path[-1]
 
     def printdebug(self):
-        print "URI: [" + self.uri + "]"
-        print "Base: [" + self.base + "]"
-        print "Protocol: [" + self.protocol + "]"
-        print "Server: [" + self.server + "]"
-        print "Port: [" + self.port + "]"
-        print "Path: [" + self.path + "]"
-        print "Language: [" + self.lang + "]"
-        print "Forced Language: [" + str(self.force_lang) + "]"
-        print "ID: [" + str(self.id) + "]"
-        print "Code [" + str(self.code) + "]"
-        print "Format [" + str(self.format) + "]"
-        print "Filename: [" + self.filename + "]"
-        print "Parameter: [" + self.parameter + "]"
-        print "Anchor: [" + self.anchor + "]"
+        print "URI: [%s]"      % self.uri
+        print "Base: [%s]"     % self.base
+        print "Protocol: [%s]" % self.protocol
+        print "Server: [%s]"   % self.server
+        print "Port: [%s]"     % self.port
+        print "Path: [%s]"     % self.path
+        print "Language: [%s]" % self.lang
+        print "Forced Language: [%s]" % self.force_lang
+        print "ID: [%s]"       % self.id
+        print "Code [%s]"      % self.code
+        print "Format [%s]"    % self.format
+        print "Filename: [%s]" % self.filename
+        print "Parameter: [%s]"% self.parameter
+        print "Anchor: [%s]"   % self.anchor
 
 
 if __name__=='__main__':
     import sys
 
     foo = URI(sys.argv[1])
-    #'http://localhost:8000/EN/editdoc/1/home?docid=1#foo')
+    #foo = URI('http://localhost:8000/EN/editdoc/1/home?docid=1#foo')
     foo.printdebug()
