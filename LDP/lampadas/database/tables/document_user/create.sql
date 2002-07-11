7da1fa1eb38f06383eb456f5a63aeedcc968e332
david <>
2002-07-11 14:49:34 +0000

    missed a username field, made them all char(40)

--- LDP/lampadas/database/tables/document_user/create.sql
+++ LDP/lampadas/database/tables/document_user/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE document_user (
 	doc_id		INT4		NOT NULL	REFERENCES document(doc_id),
-	username	CHAR(20)	NOT NULL	REFERENCES username(username),
+	username	CHAR(40)	NOT NULL	REFERENCES username(username),
 	role_code	CHAR(12)	NOT NULL	REFERENCES role(role_code),
 	email		TEXT,
 	active		BOOLEAN		NOT NULL,
