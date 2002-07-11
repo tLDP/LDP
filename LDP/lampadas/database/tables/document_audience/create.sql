394f3be1cc3809371bf5cd7df26d913278dfdb84
david <>
2002-07-11 03:50:14 +0000

    remove audience tables

--- LDP/lampadas/database/tables/document_audience/create.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE document_audience (
-	audience		CHAR(12)	NOT NULL,
-	doc_id			INT4		NOT NULL,
-	
-	PRIMARY KEY (doc_id, audience)
-);
--- LDP/lampadas/database/tables/document_audience/permissions.sql
+++
@@ -1,1 +0,0 @@
-GRANT ALL ON document_audience TO "www-data";
