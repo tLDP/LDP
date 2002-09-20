f822507e14d0e55093a48ffe28989fb9b8ca7d19
david <>
2002-09-20 03:26:33 +0000

    Table changes -- consistency.

---
+++ LDP/lampadas/database/tables/document_rating/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE document_rating
+(
+	doc_id			INT4		NOT NULL	REFERENCES document(doc_id),
+	username		CHAR(40)	NOT NULL	REFERENCES username(username),
+	rating			INT4		NOT NULL,
+	created			TIMESTAMP	NOT NULL DEFAULT now(),
+	updated			TIMESTAMP	NOT NULL DEFAULT now(),
+
+	PRIMARY KEY (doc_id, username)
+);
---
+++ LDP/lampadas/database/tables/document_rating/insert.m4
@@ -0,0 +1,2 @@
+m4_define(insert, [INSERT INTO document_rating(doc_id, username, rating)
+VALUES ($1, '$2', $3);])m4_dnl
