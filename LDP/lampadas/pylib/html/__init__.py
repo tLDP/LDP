f5a5a48e766aa9f9c2e780f5ae96e645239ee148
david <>
2002-10-20 04:01:13 +0000

    New html generating classes, a beginning.

---
+++ LDP/lampadas/pylib/html/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package provides classes for HTML generation.
+These classes are designed to be used as "decorators".
+Each class generates HTML for one of the persistence
+classes.
+"""
+
+from document import DocumentView, DocumentEdit 
+
