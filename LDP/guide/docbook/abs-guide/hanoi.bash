4fa00f10c4fb9a436bb52a01b1464f6f0584e34a
gferg <>
2004-01-05 13:21:34 +0000

    new

---
+++ LDP/guide/docbook/abs-guide/hanoi.bash
@@ -0,0 +1,99 @@
+#! /bin/bash
+#
+# The Towers Of Hanoi
+# Bash script
+# Copyright (C) 2000 Amit Singh. All Rights Reserved.
+# http://hanoi.kernelthread.com
+#
+# Last tested under bash version 2.05b.0(13)-release
+#
+#  Used in "Advanced Bash Scripting Guide"
+#+ with permission of script author.
+#  Slightly modified and commented by ABS author.
+
+#=================================================================#
+#  The Tower of Hanoi is an old mathematical puzzle.
+#  There are three vertical posts set in a base.
+#  The first post has a set of annular rings stacked on it.
+#  These rings are flat disks with a hole drilled out of the center,
+#+ so they can slip over the posts.
+#  The rings have different diameters, and they stack in descending
+#+ order, according to size.
+#  The smallest ring is on top, and the largest on the bottom.
+#
+#  The task is to transfer the stack of rings
+#+ to one of the other posts.
+#  You can move only one ring at a time to another post.
+#  You are permitted to move rings back to the original post.
+#  You may place a smaller ring atop a larger one,
+#+ but *not* vice versa.
+#  Again, it is forbidden to place a larger ring atop a smaller one.
+#
+#  For a small number of rings, only a few moves are required.
+#+ For each additional ring,
+#+ the required number of moves approximately doubles,
+#+ and the "strategy" becomes increasingly complicated.
+#
+#  For more information, see http://hanoi.kernelthread.com.
+#
+#
+#         ...                   ...                    ...
+#         | |                   | |                    | |
+#        _|_|_                  | |                    | |
+#       |_____|                 | |                    | |
+#      |_______|                | |                    | |
+#     |_________|               | |                    | |
+#    |___________|              | |                    | |
+#   |             |             | |                    | |
+# .--------------------------------------------------------------.
+# |**************************************************************|
+#          #1                   #2                      #3
+#
+#=================================================================#
+
+
+E_NOPARAM=66  # No parameter passed to script.
+E_BADPARAM=67 # Illegal number of disks passed to script.
+Moves=        # Global variable holding number of moves.
+              # Modifications to original script.
+
+dohanoi() {   # Recursive function.
+    case $1 in
+    0)
+        ;;
+    *)
+        dohanoi "$(($1-1))" $2 $4 $3
+        echo move $2 "-->" $3
+	let "Moves += 1"  # Modification to original script.
+        dohanoi "$(($1-1))" $4 $3 $2
+        ;;
+    esac
+}
+
+case $# in
+1)
+    case $(($1>0)) in     # Must have at least one disk.
+    1)
+        dohanoi $1 1 3 2
+        echo "Total moves = $Moves"
+        exit 0;
+        ;;
+    *)
+        echo "$0: illegal value for number of disks";
+        exit $E_BADPARAM;
+        ;;
+    esac
+    ;;
+*)
+    echo "usage: $0 N"
+    echo "       Where \"N\" is the number of disks."
+    exit $E_NOPARAM;
+    ;;
+esac
+
+# Exercises:
+# ---------
+# 1) Would commands beyond this point ever be executed?
+#    Why not? (Easy)
+# 2) Explain the workings of the workings of the "dohanoi" function.
+#    (Difficult)
---
+++ LDP/guide/docbook/abs-guide/wf2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# wf2.sh: Crude word frequency analysis on a text file.
+
+# Uses 'xargs' to decompose lines of text into single words.
+# Compare this example to the "wf.sh" script that follows.
+
+
+# Check for input file on command line.
+ARGS=1
+E_BADARGS=65
+E_NOFILE=66
+
+if [ $# -ne "$ARGS" ]
+# Correct number of arguments passed to script?
+then
+  echo "Usage: `basename $0` filename"
+  exit $E_BADARGS
+fi
+
+if [ ! -f "$1" ]       # Check if file exists.
+then
+  echo "File \"$1\" does not exist."
+  exit $E_NOFILE
+fi
+
+
+
+#######################################################
+cat "$1" | xargs -n1 | \
+#  List the file, one word per line. 
+tr A-Z a-z | \
+# Shift characters to lowercase.
+sed -e 's/\.//g'  -e 's/\,//g' -e 's/ /\
+/g' | \
+#  Filter out periods and commas, and
+#+ change space between words to linefeed,
+  sort | uniq -c | sort -nr
+# Finally prefix occurrence count and sort numerically.
+#######################################################
+
+#  This does the same job as the "wf.sh" example that follows,
+#+ but a bit more ponderously, and it runs more slowly.
+
+exit 0
