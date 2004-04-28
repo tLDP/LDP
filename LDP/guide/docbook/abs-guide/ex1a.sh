011a4fca9d60203148d376e5012d084bd3b6ab55
gferg <>
2004-04-28 12:08:24 +0000

    *** empty log message ***

---
+++ LDP/guide/docbook/abs-guide/ex1a.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Proper header for a Bash script.
+
+# Cleanup, version 2
+
+# Run as root, of course.
+# Insert code here to print error message and exit if not root.
+
+LOG_DIR=var/log
+# Variables are better than hard-coded values.
+cd $LOG_DIR
+
+cat /dev/null > messages
+cat /dev/null > wtmp
+
+
+echo "Logs cleaned up."
+
+exit # The right and proper method of "exiting" from a script.
