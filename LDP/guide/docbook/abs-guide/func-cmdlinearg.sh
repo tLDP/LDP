bb68e7df303f9183066d96f0ff6f3a131f707d92
gferg <>
2005-02-09 20:54:20 +0000

    new

---
+++ LDP/guide/docbook/abs-guide/func-cmdlinearg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# func-cmdlinearg.sh
+#  Call this script with a command-line argument,
+#+ something like $0 arg1.
+
+
+func ()
+
+{
+echo "$1"
+}
+
+echo "First call to function: no arg passed."
+echo "See if command-line arg is seen."
+func
+# No! Command-line arg not seen.
+
+echo "============================================================"
+echo
+echo "Second call to function: command-line arg passed explicitly."
+func $1
+# Now it's seen!
+
+exit 0
