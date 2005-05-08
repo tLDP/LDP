26e5ae5d429a3cc3408fa8f8a50d8f3bc9a07b5b
gferg <>
2005-05-08 20:10:18 +0000

    new

---
+++ LDP/guide/docbook/abs-guide/getopt-simple.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# getopt-simple.sh
+# Author: Chris Morgan
+# Used in the ABS Guide with permission.
+
+
+getopt_simple()
+{
+    echo "getopt_simple()"
+    echo "Parameters are '$*'"
+    until [ -z "$1" ]
+    do
+      echo "Processing parameter of: '$1'"
+      if [ ${1:0:1} = '/' ]
+      then
+          tmp=${1:1}               # Strip off leading '/' . . .
+          parameter=${tmp%%=*}     # Extract name.
+          value=${tmp##*=}         # Extract value.
+          echo "Parameter: '$parameter', value: '$value'"
+          eval $parameter=$value
+      fi
+      shift
+    done
+}
+
+# Pass all options to getopt_simple().
+getopt_simple $*
+
+echo "test is '$test'"
+echo "test2 is '$test2'"
+
+exit 0
+
+---
+
+sh getopt_example.sh /test=value1 /test2=value2
+
+Parameters are '/test=value1 /test2=value2'
+Processing parameter of: '/test=value1'
+Parameter: 'test', value: 'value1'
+Processing parameter of: '/test2=value2'
+Parameter: 'test2', value: 'value2'
+test is 'value1'
+test2 is 'value2'
---
+++ LDP/guide/docbook/abs-guide/iscan.sh
@@ -0,0 +1,66 @@
+#! /bin/sh
+## Duplicate DaveG's ident-scan thingie using netcat. Oooh, he'll be p*ssed.
+## Args: target port [port port port ...]
+## Hose stdout _and_ stderr together.
+##
+##  Advantages: runs slower than ident-scan, giving remote inetd less cause
+##+ for alarm, and only hits the few known daemon ports you specify.
+##  Disadvantages: requires numeric-only port args, the output sleazitude,
+##+ and won't work for r-services when coming from high source ports.
+# Script author: Hobbit &lt;hobbit@avian.org&gt;
+# Used in ABS Guide with permission.
+
+# ---------------------------------------------------
+E_BADARGS=65       # Need at least two args.
+TWO_WINKS=2        # How long to sleep.
+THREE_WINKS=3
+IDPORT=113         # Authentication "tap ident" port.
+RAND1=999
+RAND2=31337
+TIMEOUT0=9
+TIMEOUT1=8
+TIMEOUT2=4
+# ---------------------------------------------------
+
+case "${2}" in
+  "" ) echo "Need HOST and at least one PORT." ; exit $E_BADARGS ;;
+esac
+
+# Ping 'em once and see if they *are* running identd.
+nc -z -w $TIMEOUT0 "$1" $IDPORT || { echo "Oops, $1 isn't running identd." ; exit 0 ; }
+#  -z scans for listening daemons.
+#     -w $TIMEOUT = How long to try to connect.
+
+# Generate a randomish base port.
+RP=`expr $$ % $RAND1 + $RAND2`
+
+TRG="$1"
+shift
+
+while test "$1" ; do
+  nc -v -w $TIMEOUT1 -p ${RP} "$TRG" ${1} < /dev/null > /dev/null &
+  PROC=$!
+  sleep $THREE_WINKS
+  echo "${1},${RP}" | nc -w $TIMEOUT2 -r "$TRG" $IDPORT 2>&1
+  sleep $TWO_WINKS
+
+# Does this look like a lamer script or what . . . ?
+# ABS Guide author comments: "It ain't really all that bad,
+#+                            rather clever, actually."
+
+  kill -HUP $PROC
+  RP=`expr ${RP} + 1`
+  shift
+done
+
+exit $?
+
+#  Notes:
+#  -----
+
+#  Try commenting out line 30 and running this script
+#+ with "localhost.localdomain 25" as arguments.
+
+#  For more of Hobbit's 'nc' example scripts,
+#+ look in the documentation:
+#+ the /usr/share/doc/nc-X.XX/scripts directory.
