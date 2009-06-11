63d2778b9a45d0db8a351fa17ac5f392d77acff0
pbldp <>
2009-06-11 16:59:18 +0000

    Add generate helpers

---
+++ LDP/users/Peter-Bieringer/generate-all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Generate all available howtos
+
+list="Linux+IPv6-HOWTO.sgml Linux+IPv6-HOWTO.de.sgml Linux+IPv6-HOWTO.fr.sgml"
+
+for lyx in $list; do
+	./generate.sh $lyx
+done
+
+echo -en "\a"
