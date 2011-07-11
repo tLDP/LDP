c5a622cb65207d33e8aa783ce8ac8007236b758f
pbldp <>
2011-07-11 18:43:29 +0000

    Fix minor issue

--- LDP/users/Peter-Bieringer/generate-all.sh
+++ LDP/users/Peter-Bieringer/generate-all.sh
@@ -2,7 +2,7 @@
 
 # Generate all available howtos
 
-list="Linux+IPv6-HOWTO.sgml Linux+IPv6-HOWTO.de.sgml Linux+IPv6-HOWTO.fr.sgml"
+list="Linux+IPv6-HOWTO.sgml Linux+IPv6-HOWTO.de.sgml Linux+IPv6-HOWTO.fr.sgml Linux+IPv6-HOWTO.pt_BR.sgml"
 
 for lyx in $list; do
 	./generate.sh $lyx
