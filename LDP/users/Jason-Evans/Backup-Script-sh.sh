77d607809abda245d8c39c7337acdb85d8c71889
Jason S. Evans <jsevans@youvegotthe.info>
2016-02-24 14:30:19 +0100

    added Linux-Complete-Backup-and-Recovery-HOWTO-REWRITE.adoc and Backup-Script-sh.sh

---
+++ LDP/users/Jason-Evans/Backup-Script-sh.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+####################################
+#
+# Backup to NFS mount script.
+#
+####################################
+
+# What to backup. 
+backup_files="/home /var/spool/mail /etc /root /boot /opt"
+
+# Where to backup to.
+dest="/mnt/backup"
+
+# Create archive filename.
+day=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
